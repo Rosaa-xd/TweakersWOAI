fcf8c3574e15d061664075138769137c5e616fcd
Heijkoop, Marie-Rose M.M <m.heijkoop@student.fontys.nl>
2016-06-15 22:08:23 +0200

    Optimized Views

--- Tweakers/Tweakers/Controllers/CategoryController.cs
+++ Tweakers/Tweakers/Controllers/CategoryController.cs
@@ -21,112 +21,15 @@ namespace Tweakers.Controllers
         /// <param name="id"></param>
         /// <returns>
         /// View(Category.ReturnAllSubCategories(id))
-        /// RedirectToAction("ProductCat" + id, "Product")
+        /// RedirectToAction("ProductCat", "Product")
         /// </returns>
-        private ActionResult SubCategory(int id)
+        public ActionResult Cat(int id)
         {
             if (Category.ReturnAllSubCategories(id).Count != 0)
             {
                 return View(Category.ReturnAllSubCategories(id));
             }
-            return RedirectToAction("ProductCat" + id, "Product");
+            return RedirectToAction("ProductCat", "Product", new {id});
         }
-
-        #region Categories
-        public ActionResult Cat1()
-        {
-            return SubCategory(1);
-        }
-
-        public ActionResult Cat2()
-        {
-            return SubCategory(2);
-        }
-
-        public ActionResult Cat3()
-        {
-            return SubCategory(3);
-        }
-
-        public ActionResult Cat4()
-        {
-            return SubCategory(4);
-        }
-
-        public ActionResult Cat5()
-        {
-            return SubCategory(5);
-        }
-
-        public ActionResult Cat6()
-        {
-            return SubCategory(6);
-        }
-
-        public ActionResult Cat7()
-        {
-            return SubCategory(7);
-        }
-
-        public ActionResult Cat8()
-        {
-            return SubCategory(8);
-        }
-
-        public ActionResult Cat9()
-        {
-            return SubCategory(9);
-        }
-
-        public ActionResult Cat10()
-        { 
-            return SubCategory(10);
-        }
-
-        public ActionResult Cat11()
-        {
-            return SubCategory(11);
-        }
-
-        public ActionResult Cat12()
-        {
-            return SubCategory(12);
-        }
-
-        public ActionResult Cat13()
-        {
-            return SubCategory(13);
-        }
-
-        public ActionResult Cat14()
-        {
-            return SubCategory(14);
-        }
-
-        public ActionResult Cat15()
-        {
-            return SubCategory(15);
-        }
-
-        public ActionResult Cat16()
-        {
-            return SubCategory(16);
-        }
-
-        public ActionResult Cat17()
-        {
-            return SubCategory(17);
-        }
-
-        public ActionResult Cat18()
-        {
-            return SubCategory(18);
-        }
-
-        public ActionResult Cat19()
-        {
-            return SubCategory(19);
-        }
-        #endregion
     }
 }
--- Tweakers/Tweakers/Controllers/ProductController.cs
+++ Tweakers/Tweakers/Controllers/ProductController.cs
@@ -9,158 +9,28 @@ namespace Tweakers.Controllers
 {
     public class ProductController : Controller
     {
-        #region ProductCategories
         /// <summary>
-        /// Method to automatically generate the right view for the corresponding category
+        /// Method to automatically generate all the products within a category
         /// </summary>
         /// <param name="id"></param>
         /// <returns>View(Models.Product.FindAllProductsInCategory(id)</returns>
-        public ActionResult GenerateProducts(int id)
+        public ActionResult ProductCat(int id)
         {
-            return View(Product.FindAllProductsInCategory(id));
+            return View(Models.Product.FindAllProductsInCategory(id));
         }
 
         /// <summary>
-        /// All ActionResults for the ProductCategories
-        /// </summary>
-        public ActionResult ProductCat2()
-        {
-            return GenerateProducts(2);
-        }
-
-        public ActionResult ProductCat3()
-        {
-            return GenerateProducts(3);
-        }
-
-        public ActionResult ProductCat4()
-        {
-            return GenerateProducts(4);
-        }
-
-        public ActionResult ProductCat5()
-        {
-            return GenerateProducts(5);
-        }
-
-        public ActionResult ProductCat6()
-        {
-            return GenerateProducts(6);
-        }
-
-        public ActionResult ProductCat8()
-        {
-            return GenerateProducts(8);
-        }
-
-        public ActionResult ProductCat9()
-        {
-            return GenerateProducts(9);
-        }
-
-        public ActionResult ProductCat10()
-        {
-            return GenerateProducts(10);
-        }
-
-        public ActionResult ProductCat11()
-        {
-            return GenerateProducts(11);
-        }
-
-        public ActionResult ProductCat13()
-        {
-            return GenerateProducts(13);
-        }
-
-        public ActionResult ProductCat15()
-        {
-            return GenerateProducts(15);
-        }
-
-        public ActionResult ProductCat16()
-        {
-            return GenerateProducts(16);
-        }
-
-        public ActionResult ProductCat17()
-        {
-            return GenerateProducts(17);
-        }
-
-        public ActionResult ProductCat18()
-        {
-            return GenerateProducts(18);
-        }
-
-        public ActionResult ProductCat19()
-        {
-            return GenerateProducts(19);
-        }
-        #endregion
-
-        #region Products
-        /// <summary>
-        /// These are the ActionResults of all the Product Views.
+        /// These are the ActionResults of the Product View.
         /// They only contain a return View statement which redirects to the ShopPricesProduct
-        /// and pass along the right product from the Dictionary.
-        /// When a new product is added, only a new ActionResult and a View has to be made.
-        /// ActionResult Product1() and Product1.cshtml can be used as templates for the new products.
+        /// and pass along the right product from the Dictionary..
         /// </summary>
         /// <param name="id"></param>
         /// <returns>View("ShopPricesProduct", Dictionaries.Products[id]</returns>
-        public ActionResult Product1(int id)
+        public ActionResult Product(int id)
         {
             return View("ShopPricesProduct", Dictionaries.Products[id]);
         }
-
-        public ActionResult Product2(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product3(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product4(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product5(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product6(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product7(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product8(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product9(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-
-        public ActionResult Product10(int id)
-        {
-            return View("ShopPricesProduct", Dictionaries.Products[id]);
-        }
-        #endregion
-
-        #region productViews
+        
         public ActionResult ShopPricesProduct(int id)
         {
             return View(Dictionaries.Products[id]);
@@ -175,6 +45,5 @@ namespace Tweakers.Controllers
         {
             return View(Dictionaries.Products[id]);
         }
-        #endregion
     }
 }
