65572b0cc145b4849ed83be97049f73c8c34503c
Rosaa-xd <github@shadowphoenix.nl>
2016-06-19 01:43:36 +0200

    Added Create ProductReview, doesn't work yet

--- Tweakers/Tweakers/Controllers/ProductReviewController.cs
+++ Tweakers/Tweakers/Controllers/ProductReviewController.cs
@@ -3,21 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tweakers.Models;
 
 namespace Tweakers.Controllers
 {
     public class ProductReviewController : Controller
     {
-        // GET: ProductReview
-        public ActionResult Index()
+        private List<ProductReview> prs = new List<ProductReview>();
+        private User user = Models.User.FindByName(System.Web.HttpContext.Current.User.Identity.Name);
+
+        [Authorize]
+        public ActionResult WriteReview(int id)
         {
+            ProductReview pr = new ProductReview(user, Dictionaries.Products[id], DateTime.MinValue, 0, null);
+            prs.Add(pr);
             return View();
         }
 
-        [Authorize]
-        public ActionResult WriteReview()
+        [HttpPost]
+        public ActionResult WriteReview(int score, string explanation)
         {
-            return View();
+            ProductReview pr = prs.Find(review => review.User.ID.Equals(user.ID));
+            pr.Score = score;
+            pr.Explanation = explanation;
+            ProductReview.SaveProductReview(pr);
+            return RedirectToAction("Product", "Product", pr.Product.ID);
         }
     }
 }
